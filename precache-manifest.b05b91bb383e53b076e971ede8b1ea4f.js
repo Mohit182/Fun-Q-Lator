9b8e95835060e0a1dea86ac4667b89e28349546a
MOHIT GUPTA <mohitguptatech18@gmail.com>
2022-07-16 23:06:30 +0530

    Updates

---
+++ precache-manifest.b05b91bb383e53b076e971ede8b1ea4f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "c01fb26ef3665455f56d",
+    "url": "/Fun-Q-Lator/static/css/main.cb4576be.chunk.css"
+  },
+  {
+    "revision": "c01fb26ef3665455f56d",
+    "url": "/Fun-Q-Lator/static/js/main.c01fb26e.chunk.js"
+  },
+  {
+    "revision": "feba993bef7ae26f9c7e",
+    "url": "/Fun-Q-Lator/static/css/1.264e4ded.chunk.css"
+  },
+  {
+    "revision": "feba993bef7ae26f9c7e",
+    "url": "/Fun-Q-Lator/static/js/1.feba993b.chunk.js"
+  },
+  {
+    "revision": "754a37d713c350f6cd92",
+    "url": "/Fun-Q-Lator/static/js/runtime~main.754a37d7.js"
+  },
+  {
+    "revision": "7c63dc11e98694421cc20b5bcc6d55ba",
+    "url": "/Fun-Q-Lator/static/media/heads.7c63dc11.svg"
+  },
+  {
+    "revision": "3d90d17eb73999ff69d25b151f4b863f",
+    "url": "/Fun-Q-Lator/static/media/tails.3d90d17e.svg"
+  },
+  {
+    "revision": "ec33a8375862c4b93241b7bb4aea4c7a",
+    "url": "/Fun-Q-Lator/static/media/video6.ec33a837.mp4"
+  },
+  {
+    "revision": "2e679746732912c1c249118fa5876923",
+    "url": "/Fun-Q-Lator/static/media/underweight.2e679746.png"
+  },
+  {
+    "revision": "aaca6e71b58d29192350e03b17ebf27b",
+    "url": "/Fun-Q-Lator/static/media/healthy.aaca6e71.png"
+  },
+  {
+    "revision": "7ba81ac349488ffedb78386ec031baca",
+    "url": "/Fun-Q-Lator/static/media/overweight.7ba81ac3.png"
+  },
+  {
+    "revision": "5b769ddeccb2c6388f97c24c47911657",
+    "url": "/Fun-Q-Lator/index.html"
+  }
+];
